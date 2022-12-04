b6cf7be4297342f05e24674cbc68c327394840d0
iq <ian@ianq.ai>
2022-12-04 15:18:26 -0800

    day 4 shitshow

--- src/day4.rs
+++ src/day4.rs
@@ -55,8 +55,39 @@ pub fn problem1(f_name: &str) -> usize {
             return (range1, range2);
         })
         .map(|ranges| {
-            let (r1, r2) :(Range, Range) = (ranges.0, ranges.1);
+            let (r1, r2): (Range, Range) = (ranges.0, ranges.1);
             return (r1.start <= r2.start && r1.end >= r2.end) || (r2.start <= r1.start && r2.end >= r1.end);
+        })
+        .filter(|x| *x)
+        .collect()
+        ;
+
+    return filtered_results.len();
+}
+
+// Too lazy to use Range. Just doing it with raw values
+pub fn problem2(f_name: &str) -> usize {
+    let split_values = read_file(f_name);
+
+    let filtered_results: Vec<bool> = split_values.iter()
+        // Generate the two ranges Range1(start1, end1) and Range2(start2, end2)
+        .map(|line| {
+            let split_ln: Vec<i32> = line.split(" ").map(|v| v.parse::<i32>().unwrap()).collect();
+            let (s1, e1, s2, e2) = match split_ln[..] {
+                [s1, e1, s2, e2, ..] => (s1, e1, s2, e2),
+                _ => unreachable!(),
+            };
+            // Is contained in other
+
+            let superset: bool = (s1 <= s2 && e2 <= e1) ||
+                (s2 <= s1&& e1 <= e2);
+
+            let intersect: bool = (s1 <= s2 && s2 <= e1) ||
+                (s1 <= e2 && e2 <= e1) ||
+                (s2 <= s1 && s1 <= e2) ||
+                (s2 <= e1 && e1 <= e2);
+
+            return superset || intersect;
         })
         .filter(|x| *x)
         .collect()
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     // let result = day3::part1_calculate_priorities("./data_folder/day_3.txt");
     // println!("Day 3: Part 1: {}", result);
 
-    println!("{}", day4::problem1("./data_folder/day_4.txt"));
+    println!("{}", day4::problem2("./data_folder/day_4.txt"));
 }
